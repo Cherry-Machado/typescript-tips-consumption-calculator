feec2ea6203d99f3fa8ca3a3b6d02ae791cd6ff5
Cherry-Machado <cherry5467@gmail.com>
2026-01-28 14:31:02 -0300

    Creating basic Menu and Consumption UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { menuItems } from "./data/db"
+
 function App() {
+
+  console.log(menuItems);
   
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Tips and Consumption Calculator</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+
+        <div>
+          <h2>Consumption</h2>
+        </div>
+      </main>
     </>
   )
 }
