79882ba2f41b8e90d782402eba222f02670bc730
Cherry-Machado <cherry5467@gmail.com>
2026-01-29 20:04:08 -0300

    updating OrderTotals component

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,5 +1,26 @@
 export default function OrderTotals() {
   return (
-    <div>OrderTotals</div>
+    <>
+        <div className="space-y-3">
+            <h2 className='font-black text-2xl'>Totals and Tips</h2>
+            <p>Subtotal amount: {''}
+                <span className="font-bold">$0</span>
+            </p>
+
+            <p>Suggested Tip Amounts: {''}
+                <span className="font-bold">10%: $0</span>
+                <span className="font-bold">15%: $0</span>
+                <span className="font-bold">20%: $0</span>
+            </p>
+
+            <p>Total with Tip: {''}
+                <span className="font-bold">$0</span>
+            </p>
+        </div>
+
+        <button>
+            Complete Order
+        </button>
+    </>
   )
 }
